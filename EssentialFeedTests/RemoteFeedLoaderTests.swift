a28d0d7230b0bb2ca0a3785ebf035bb2747a3ae9
beccahalford <rebeccawoodmanhalford@hotmail.co.uk>
2023-02-08 16:36:38 +0000

    replace stubbing by capturing values instead

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -40,9 +40,10 @@ class RemoteFeedLoaderTests: XCTestCase {
         let (sut, client) = makeSUT()
         var capturedErrors = [RemoteFeedLoader.Error]()
         
-        client.error = NSError(domain: "test", code: 0)
         sut.load { capturedErrors.append($0) }
         
+        let clientError = NSError(domain: "test", code: 0)
+        client.completions[0](clientError)
         XCTAssertEqual(capturedErrors , [.connectivity])
     }
     
@@ -56,12 +57,10 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
-        var error: Error?
+        var completions = [(Error) -> Void]()
         
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            if let error = error {
-                completion(error)
-            }
+            completions.append(completion)
             requestedURLs.append(url)
         }
     }
