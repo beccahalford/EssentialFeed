3ef0f28a91b01c908832c24d64d6348864b73e4b
beccahalford <rebeccawoodmanhalford@hotmail.co.uk>
2023-02-08 14:32:31 +0000

    remote feed loader does not request data upon creation

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Rebecca Woodman-Halford on 08/02/2023.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+                
+        XCTAssertNil(client.requestedURL)
+    }
+}
