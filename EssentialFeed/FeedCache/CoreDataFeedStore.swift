4f6ae4de3c59a52f8cb8bef1a96bbc3a162ec71c
beccahalford <rebeccawoodmanhalford@hotmail.co.uk>
2023-09-19 16:09:39 +0100

    CoreDataFeedStore.retrieve() delivers empty on empty cache

---
+++ EssentialFeed/FeedCache/CoreDataFeedStore.swift
@@ -0,0 +1,25 @@
+//
+//  CoreDataFeedStore.swift
+//  EssentialFeed
+//
+//  Created by Rebecca Woodman-Halford on 19/09/2023.
+//
+
+import Foundation
+
+public final class CoreDataFeedStore: FeedStore {
+    public init() {}
+
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
+
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+
+    }
+
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+
+    }
+
+}
