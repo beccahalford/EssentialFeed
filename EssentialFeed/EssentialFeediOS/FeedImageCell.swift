901247b8cc1799a2e054f546710976ca1b3da90e
beccahalford <rebeccawoodmanhalford@hotmail.co.uk>
2023-12-08 16:48:39 +0000

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Rebecca Woodman-Halford on 08/12/2023.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
