4ed4f5309fe10a6c394a43ddcf7a4a785fa13c97
beccahalford <rebeccawoodmanhalford@hotmail.co.uk>
2023-12-30 10:56:10 +0000

    Move FeedErrorViewModel to separate file

--- EssentialFeed/EssentialFeed/FeedPresentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeed/FeedPresentation/FeedPresenter.swift
@@ -7,17 +7,6 @@
 
 
 
-public struct FeedErrorViewModel {
-    public let message: String?
-
-    static var noError: FeedErrorViewModel {
-        return FeedErrorViewModel(message: nil)
-    }
-    
-    static func error(message: String) -> FeedErrorViewModel {
-        return FeedErrorViewModel(message: message)
-    }
-}
 
 public protocol FeedLoadingView {
     func display(_ viewModel: FeedLoadingViewModel)
