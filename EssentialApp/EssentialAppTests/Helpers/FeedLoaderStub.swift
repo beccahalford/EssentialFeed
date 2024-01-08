d109563caf072c4cb244bda7123603e279cb2b1a
beccahalford <rebeccawoodmanhalford@hotmail.co.uk>
2024-01-08 10:39:50 +0000

    Extract FeedLoaderStub into a shared scope to remove duplication

---
+++ EssentialApp/EssentialAppTests/Helpers/FeedLoaderStub.swift
@@ -0,0 +1,20 @@
+//
+//  FeedLoaderStub.swift
+//  EssentialAppTests
+//
+//  Created by Rebecca Woodman-Halford on 08/01/2024.
+//
+
+@testable import EssentialFeed
+
+class FeedLoaderStub: FeedLoader {
+    private let result: FeedLoader.Result
+    
+    init(result: FeedLoader.Result) {
+        self.result = result
+    }
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        completion(result)
+    }
+}
