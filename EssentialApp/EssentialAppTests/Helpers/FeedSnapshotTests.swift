cfac9394ccae8b2998ba92d91084f003af1cd6f7
beccahalford <rebeccawoodmanhalford@hotmail.co.uk>
2024-01-12 13:30:05 +0000

    Record empty feed snapshot

---
+++ EssentialApp/EssentialAppTests/Helpers/FeedSnapshotTests.swift
@@ -0,0 +1,14 @@
+//
+//  FeedSnapshotTests.swift
+//  EssentialFeedTests
+//
+//  Created by Rebecca Woodman-Halford on 12/01/2024.
+//
+
+import XCTest
+
+final class FeedSnapshotTests: XCTestCase {
+
+
+
+}
