bd935f94121c808a2ba4eb3d4177a4472059db98
beccahalford <rebeccawoodmanhalford@hotmail.co.uk>
2024-01-19 12:25:36 +0000

    Replace RemoteFeedLoader with generic RemoteLoader to remove duplication

--- EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -7,45 +7,53 @@
 
 import Foundation
 
-public final class RemoteFeedLoader: FeedLoader {
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    public typealias Result = FeedLoader.Result
-    
-    private let url: URL
-    private let client: HTTPClient
-    
-    public init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    public func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
+public typealias RemoteFeedLoader = RemoteLoader<[FeedImage]>
 
-            switch result {
-            case let .success(data, response):
-                completion(RemoteFeedLoader.map(data, from: response))
-            case .failure:
-                completion(.failure(Error.connectivity))
-            }
-        }
-    }
-    
-    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
-        do {
-            let items = try FeedItemsMapper.map(data, from: response)
-            return .success(items)
-//            return .success(items.toModels())
-        } catch {
-            return .failure(error)
-        }
+public extension RemoteFeedLoader {
+    convenience init(url: URL, client: HTTPClient) {
+        self.init(url: url, client: client, mapper: FeedItemsMapper.map)
     }
 }
+
+//public final class RemoteFeedLoader: FeedLoader {
+//    public enum Error: Swift.Error {
+//        case connectivity
+//        case invalidData
+//    }
+//    
+//    public typealias Result = FeedLoader.Result
+//    
+//    private let url: URL
+//    private let client: HTTPClient
+//    
+//    public init(url: URL, client: HTTPClient) {
+//        self.url = url
+//        self.client = client
+//    }
+//    
+//    public func load(completion: @escaping (Result) -> Void) {
+//        client.get(from: url) { [weak self] result in
+//            guard self != nil else { return }
+//
+//            switch result {
+//            case let .success(data, response):
+//                completion(RemoteFeedLoader.map(data, from: response))
+//            case .failure:
+//                completion(.failure(Error.connectivity))
+//            }
+//        }
+//    }
+//    
+//    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+//        do {
+//            let items = try FeedItemsMapper.map(data, from: response)
+//            return .success(items)
+////            return .success(items.toModels())
+//        } catch {
+//            return .failure(error)
+//        }
+//    }
+//}
 
 //private extension Array where Element == RemoteFeedItem {
 //    func toModels() -> [FeedImage] {
