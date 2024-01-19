fd8daacc14312350a3c83765a0ccb53873d0c485
beccahalford <rebeccawoodmanhalford@hotmail.co.uk>
2024-01-19 13:53:29 +0000

    comment unused code

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -122,3 +122,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 }
 
 //extension RemoteLoader: FeedLoader where Resource == [FeedImage] {}
+
+//public typealias RemoteFeedLoader = RemoteLoader<[FeedImage]>
+//
+//public extension RemoteFeedLoader {
+//    convenience init(url: URL, client: HTTPClient) {
+//        self.init(url: url, client: client, mapper: FeedItemsMapper.map)
+//    }
+//}
+
+//public typealias RemoteImageCommentsLoader = RemoteLoader<[ImageComment]>
+//
+//public extension RemoteImageCommentsLoader {
+//    convenience init(url: URL, client: HTTPClient) {
+//        self.init(url: url, client: client, mapper: ImageCommentsMapper.map)
+//    }
+//}
