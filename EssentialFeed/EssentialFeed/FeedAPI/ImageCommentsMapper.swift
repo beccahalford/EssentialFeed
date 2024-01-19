861aa1978b43b0fc14425eaf630f5899854a236c
beccahalford <rebeccawoodmanhalford@hotmail.co.uk>
2024-01-19 10:44:16 +0000

    Duplicate RemoteFeedLoader as RemoteImageCommentsLoader

---
+++ EssentialFeed/EssentialFeed/FeedAPI/ImageCommentsMapper.swift
@@ -0,0 +1,22 @@
+//
+//  ImageCommentsMapper.swift
+//  EssentialFeed
+//
+//  Created by Rebecca Woodman-Halford on 19/01/2024.
+//
+
+import Foundation
+
+final class ImageCommentsMapper {
+    private struct Root: Decodable {
+        let items: [RemoteFeedItem]
+    }
+
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+        guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            throw RemoteImageCommentsLoader.Error.invalidData
+        }
+
+        return root.items
+    }
+}
