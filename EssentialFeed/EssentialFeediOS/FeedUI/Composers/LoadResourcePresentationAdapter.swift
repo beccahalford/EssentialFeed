bed08f04c90013344c70d079eea0087d3bb1f6de
beccahalford <rebeccawoodmanhalford@hotmail.co.uk>
2024-01-26 10:24:41 +0000

    Replace FeedViewControllerDelegate with a closure

--- EssentialFeed/EssentialFeediOS/FeedUI/Composers/LoadResourcePresentationAdapter.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Composers/LoadResourcePresentationAdapter.swift
@@ -37,11 +37,13 @@ final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     }
 }
 
+/*
 extension LoadResourcePresentationAdapter: FeedViewControllerDelegate {
     func didRequestFeedRefresh() {
         loadResource()
     }
 }
+ */
 
 extension LoadResourcePresentationAdapter: FeedImageCellControllerDelegate {
     func didRequestImage() {
