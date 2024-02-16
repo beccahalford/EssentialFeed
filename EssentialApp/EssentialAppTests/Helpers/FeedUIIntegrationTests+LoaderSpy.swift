12026513d337816bf63876bf1156a8a0fda845c8
beccahalford <rebeccawoodmanhalford@hotmail.co.uk>
2024-02-16 11:22:03 +0000

    Load more items on will display LoadMoreCell

--- EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+LoaderSpy.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+LoaderSpy.swift
@@ -26,6 +26,8 @@ extension FeedUIIntegrationTests {
 //            feedRequests.append(completion)
 //        }
         
+        private(set) var loadMoreCallCount = 0
+        
         func loadPublisher() -> AnyPublisher<Paginated<FeedImage>, Error> {
             let publisher = PassthroughSubject<Paginated<FeedImage>, Error>()
             feedRequests.append(publisher)
@@ -34,7 +36,9 @@ extension FeedUIIntegrationTests {
         
         func completeFeedLoading(with feed: [FeedImage] = [], at index: Int = 0) {
 //            feedRequests[index](.success(feed))
-            feedRequests[index].send(Paginated(items: feed))
+            feedRequests[index].send(Paginated(items: feed, loadMore: { [weak self] _ in
+                self?.loadMoreCallCount += 1
+            }))
         }
         
         func completeFeedLoadingWithError(with feed: [FeedImage] = [], at index: Int = 0) {
